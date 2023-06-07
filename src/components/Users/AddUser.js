26d06db28f964629cbe100bf82993ca28d7541a4
Duvantrick <duvanli@hotmail.es>
2023-06-07 13:03:00 +1000

    add functions

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -8,20 +8,55 @@ import UsersList from './UsersList';
 let usersList = [];
 
 const AddUser = () => {
+  const [List, setUsers] = useState(usersList);
+  const [data, setData] = useState({
+    name: '',
+    age: 0,
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (data.name.length < 3) {
+      alert('Please enter a valid name');
+    } else if (data.age === 0) {
+      alert('Please enter a valid age');
+    } else {
+      setUsers(data);
+    }
+  };
+
+  const onChange = (e, property) => {
+    const text = e.target.value;
+    setData({
+      ...data,
+      [property]: text,
+    });
+  };
+
   return (
     <div>
       <Card className={classes.card}>
-        <form onSubmit={null} className={classes.form}>
+        <form onSubmit={onSubmit} className={classes.form}>
           <label htmlFor="username"> Username </label>{' '}
-          <input id="username" type="text" value={null} onChange={null} />
+          <input
+            id="username"
+            type="text"
+            value={data.name}
+            onChange={(e) => onChange(e, 'name')}
+          />
           <label htmlFor="age"> Age(Years) </label>{' '}
-          <input id="age" type="number" value={null} onChange={null} />{' '}
+          <input
+            id="age"
+            type="number"
+            value={data.age}
+            onChange={(e) => onChange(e, 'age')}
+          />{' '}
           <Button type="submit" className={classes.submit}>
             Add User{' '}
           </Button>{' '}
         </form>{' '}
       </Card>{' '}
-      <UsersList users={usersList} />{' '}
+      <UsersList users={List} />{' '}
     </div>
   );
 };
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -4,11 +4,18 @@ import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
-	return (
-		<Card className={classes.users}>
-			<ul>{/*  Add List of Users here.  */}</ul>
-		</Card>
-	);
+  const userlist = props.users;
+  return (
+    <Card className={classes.users}>
+      <li>
+        {userlist.map((user) => (
+          <ul>
+            <h1>{user.name + user.age}</h1>
+          </ul>
+        ))}
+      </li>
+    </Card>
+  );
 };
 
 export default UsersList;
